9b693e1bc1cfd26cc07b925c6f2d5d9d68c20417
schochastics <david@schochastics.net>
2024-03-28 22:13:25 +0100

    try expect_equal

--- tests/testthat/test-timeless.R
+++ tests/testthat/test-timeless.R
@@ -14,5 +14,5 @@ test_that("chronos works with supported formats", {
 
 test_that("timezone conversion #14", {
     res <- chronos(bench_date[5], tz = "America/Los_Angeles", to_tz = "UTC")
-    expect_true(res == as.POSIXct("2017-11-26 07:34:50 UTC", tz = "UTC"))
+    expect_equal(res, as.POSIXct("2017-11-26 07:34:50", tz = "UTC"))
 })
